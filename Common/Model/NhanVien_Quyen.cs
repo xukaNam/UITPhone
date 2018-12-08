6475b68bf30fd406eb87b1675389116f908829f1
BuiThaoNam <15520503@gm.uit.edu.vn>
2018-12-08 11:26:37 +0700

    update class chuc vu va class NhanVien_Quyen

---
+++ Common/Model/NhanVien_Quyen.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
