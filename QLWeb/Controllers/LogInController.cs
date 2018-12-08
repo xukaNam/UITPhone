2b6044bc2d4162aceda93799d9d3fac0c47a2919
BÌNH NGUYỄN VĂN <15520053@gm.uit.edu.vn>
2018-12-08 11:03:25 +0700

    Log In
    Thêm phần LogIn và mã hóa MD5

---
+++ QLWeb/Controllers/LogInController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QLWeb.Models.LogIn;
+using QLWeb.Areas.Admin;
+
+namespace QLWeb.Controllers
+{
+    public class LogInController : Controller
+    {
+        // GET: LogIn
+        public ActionResult Index()
+        {
+            return View(1);
+        }
+
+        //POST : LogIn
+        [HttpPost]
+        public ActionResult Index(LogIn logIn)
+        {
+            var result = new LogIn(logIn.UserName, Encryptor.MD5Hash(logIn.PassWord));
+            int i = 1;
+            string password = Encryptor.MD5Hash("admin");
+            if (result.UserName == "admin" && result.PassWord == password)
+            {
+                i = 2;
+            }
+            else
+            {
+                i = 3;
+            }
+
+            return View(i);
+        }
+    }
+}
---
+++ QLWeb/Models/LogIn/LogIn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QLWeb.Models.LogIn
+{
+    public class LogIn
+    {
+        [Required]
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        //public bool RememberMe { get; set; }
+        public LogIn()
+        {
+
+        }
+        public LogIn(string UserName, string PassWord)
+        {
+            this.UserName = UserName;
+            this.PassWord = PassWord;
+        }
+    }
+}
